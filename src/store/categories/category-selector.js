ff7ed42062632d70ce7b204794175cb2dcf1f0b5
Josh Worden <2015JTW@gmail.com>
2022-10-06 16:09:53 -0400

    starting to transfer cart context to reducer

--- src/store/categories/category-selector.js
+++ src/store/categories/category-selector.js
@@ -1,10 +1,22 @@
+import { createSelector } from 'reselect';
 
 
-export const selectCategoriesMap = (state) => state.categories.categories
-    .reduce((accumulator, category) => {
+const selectCategoryReducer = (state) => state.categories;
+
+// this createSelector func takes 2 args: the input selector array, and the output selector function
+export const selectCategories = createSelector(
+    // input selector is what do i want as part of the parameters to produce what the selector should return back
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+
+)
+ 
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => categories.reduce((accumulator, category) => {
         const {title, items} = category;
         accumulator[title.toLowerCase()] = items;
         return accumulator;
     }, {})
-
- 
+)
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,24 @@
 import { compose, createStore, applyMiddleware } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
+
+// create own middleware
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type){
+        return next(action);
+    }
+    console.log('type', action.type);
+    console.log('payload', action.payload);
+    console.log('currentState: ', store.getState());
+
+    next(action);
+
+    console.log('next state: ', store.getState());
+
+}
 
 // create root reducer
-
-const middleWares = [logger];
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
